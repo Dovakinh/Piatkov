1bb0910c9f1f24033f7721b38de77f24a9f0773a
Dovakinh <154557721+Dovakinh@users.noreply.github.com>
2024-02-08 18:57:29 +1200

    Add files via upload

---
+++ 7 практика/точка на прямой.py
@@ -0,0 +1,11 @@
+def line(s, t):
+    x1, x2 = map(int, t.split(';'))
+    s1, s2 = map(str, s.split('+'))
+    if s1 != 'x':
+        s1 = (s1.replace('x',''))
+    a = (int(s2) / int(s1))
+    if x1 < a < x2:
+        print('True')
+    else:
+        print('False')
+line("10x+60", "1;7")
