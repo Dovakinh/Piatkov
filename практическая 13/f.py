7f8811dee1c8e3469c627ffe38db8b07a1bbed5b
Dovakinh <154557721+Dovakinh@users.noreply.github.com>
2024-04-04 22:58:57 +1200

    f.py

--- практическая 13/f.py
+++ практическая 13/f.py
@@ -1,14 +1,14 @@
 from random import randint
-a = [randint(120, 200) for i in range(30)]
-print(f'Ученики:\n{" ".join(str(i) for i in a)}')
-b = 0
-for i in range(30):
-    if a[i] >= 170:
-        b += 1
+n= 25
+a = [randint(30, 140) for i in range(n)]
+b = []
+c = []
+for i in range(25):
+    if a[i] >= 100: 
+        b.append(a[i])
     else:
-        pass
-if b >= 5:
-    print("Команду можно создать")
-else:
-    print("Команду нельзя создать")
-
+        c.append(a[i])
+bs = sum(i for i in b)
+cs = sum(i for i in c)
+print(f'Средняя масса полных людей {bs/len(b)}')
+print(f'Средняя масса остальных людей {cs/len(c)}')
