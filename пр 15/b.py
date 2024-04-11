bf745a501a443867d5c7b5a8c4e9b177ffe7eb49
Dovakinh <154557721+Dovakinh@users.noreply.github.com>
2024-04-11 21:27:40 +1200

    b.py

--- пр 15/b.py
+++ пр 15/b.py
@@ -4,9 +4,14 @@ print('Массив', end = ' ')
 print(*a)
 b = []
 for i in range(len(a)):
-    if a[i] % 2 == 0 or a[i] % 3 == 0 or a[i] % 5 == 0 or a[i] % 7 == 0:
+    if a[i] == 2 or a[i] == 3 or a[i] == 5 or a[i] == 7:
+        b.append(a[i])
+    elif a[i] <= 1 or a[i] % 2 == 0 or a[i] % 3 == 0 or a[i] % 5 == 0 or a[i] % 7 == 0:
         pass
     else:
         b.append(a[i])
-print('Массив', end = ' ')
-print(*b)
+if len(b) == 0:
+    print('Простых чисел нет')
+else:
+    print('Массив', end = ' ')
+    print(*b)
